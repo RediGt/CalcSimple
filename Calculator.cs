5cf852778abe432bb6963566e96914f58ffeb721
marinarediko <ma.rediko@gmail.com>
2020-05-06 17:42:16 +0300

    Version_01

---
+++ Calculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcSimple
+{
+    public partial class CalcSimple : Form
+    {
+        public CalcSimple()
+        {
+            InitializeComponent();
+            InitializeCalculator();
+        }
+
+        private void InitializeCalculator()
+        {
+            this.BackColor = Color.LightSeaGreen;
+
+            string buttonName = null;
+            Button button = null;
+            for (int i=0; i<10; i++)
+            {
+                buttonName = "button" + i;
+                button = (Button)this.Controls[buttonName];
+                button.Text = i.ToString();
+            }
+           
+        }
+    }
+
+}
