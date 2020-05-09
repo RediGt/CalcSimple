00e4878a016f3741a9adb9e5633fbfb1861619c9
marinarediko <ma.rediko@gmail.com>
2020-05-09 17:44:19 +0300

    Version_03

--- Calculator.Designer.cs
+++ Calculator.Designer.cs
@@ -39,6 +39,8 @@
             this.button8 = new System.Windows.Forms.Button();
             this.button9 = new System.Windows.Forms.Button();
             this.button0 = new System.Windows.Forms.Button();
+            this.buttonDecimal = new System.Windows.Forms.Button();
+            this.buttonBackspace = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -56,7 +58,7 @@
             this.Display.Font = new System.Drawing.Font("Modern No. 20", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Display.Location = new System.Drawing.Point(12, 12);
             this.Display.Name = "Display";
-            this.Display.Size = new System.Drawing.Size(314, 42);
+            this.Display.Size = new System.Drawing.Size(318, 42);
             this.Display.TabIndex = 1;
             this.Display.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -150,11 +152,35 @@
             this.button0.UseVisualStyleBackColor = true;
             this.button0.Click += new System.EventHandler(this.Button_Click);
             // 
+            // buttonDecimal
+            // 
+            this.buttonDecimal.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecimal.Location = new System.Drawing.Point(174, 307);
+            this.buttonDecimal.Name = "buttonDecimal";
+            this.buttonDecimal.Size = new System.Drawing.Size(75, 75);
+            this.buttonDecimal.TabIndex = 11;
+            this.buttonDecimal.Text = ".";
+            this.buttonDecimal.UseVisualStyleBackColor = true;
+            this.buttonDecimal.Click += new System.EventHandler(this.buttonDecimal_Click);
+            // 
+            // buttonBackspace
+            // 
+            this.buttonBackspace.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonBackspace.Location = new System.Drawing.Point(255, 64);
+            this.buttonBackspace.Name = "buttonBackspace";
+            this.buttonBackspace.Size = new System.Drawing.Size(75, 75);
+            this.buttonBackspace.TabIndex = 12;
+            this.buttonBackspace.Text = "🠔";
+            this.buttonBackspace.UseVisualStyleBackColor = true;
+            this.buttonBackspace.Click += new System.EventHandler(this.buttonBackspace_Click);
+            // 
             // CalcSimple
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(351, 450);
+            this.Controls.Add(this.buttonBackspace);
+            this.Controls.Add(this.buttonDecimal);
             this.Controls.Add(this.button0);
             this.Controls.Add(this.button9);
             this.Controls.Add(this.button8);
@@ -186,6 +212,8 @@
         private System.Windows.Forms.Button button8;
         private System.Windows.Forms.Button button9;
         private System.Windows.Forms.Button button0;
+        private System.Windows.Forms.Button buttonDecimal;
+        private System.Windows.Forms.Button buttonBackspace;
     }
 }
 
--- Calculator.cs
+++ Calculator.cs
@@ -29,6 +29,7 @@ namespace CalcSimple
                 buttonName = "button" + i;
                 button = (Button)this.Controls[buttonName];
                 button.Text = i.ToString();
+                //button.Font = new Font("Tahome", 22f);
             }
            
         }
@@ -37,6 +38,38 @@ namespace CalcSimple
         {
             Button button = (Button)sender;
             Display.Text += button.Text;
+        }
+
+        private void buttonDecimal_Click(object sender, EventArgs e)
+        {
+            bool weHaveDot = Display.Text.Contains(".");
+            if (!weHaveDot) 
+            {  
+                if (Display.Text == "")
+            {
+                Display.Text += "0.";
+            }
+            else
+                Display.Text += buttonDecimal.Text;
+            }
+          
+            
+        }
+
+        private void buttonBackspace_Click(object sender, EventArgs e)
+        {
+                string s = Display.Text;
+                if (s.Length >= 1)
+                {
+                    s = s.Substring(0, s.Length - 1);
+                }
+               // else
+               // {
+                //    s = "";
+                //}
+
+                Display.Text = s;
+
         }
     }
 
